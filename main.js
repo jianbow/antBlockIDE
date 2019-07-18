76794b7854949d903540754648df3acfbe100998
Leo <leozhuang123@gmail.com>
2019-07-17 21:36:36 -0500

    save/load/status/UI Update

--- main.js
+++ main.js
@@ -3,23 +3,46 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const { shell } = require('electron');
 
+//var fs = require('fs');
+
+//fs.readFile('arduino-manager/temp.txt', 'utf8', function (err, data) {
+//    if (err) throw err;
+//    console.log(data);
+//});
+
+
 // Open a local file in the default app
 
 //UNCOMMENT THE SHELL OPEN ITME LINE TO RUN THE BATCH FILE TO CREATE LOCAL SERVER FOR ARDUINO
 
 //shell.openItem(app.getAppPath() + '\\arduino-manager\\load_server.bat');
 
-
-
+//your code
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 //store.set('foo.bar', false);
 //console.log(store.get('foo'));
 
-
-
 //console.log(app.getPath('userData'));
+
+var obj = {
+    table: []
+};
+obj.table.push({ 1: 2, yee: 'haw' });
+var json = JSON.stringify(obj);
+var fs = require('fs');
+fs.writeFile('arduino-manager\\arduinoSettings.json', json, 'utf8', function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    //console.log('inside function');
+
+});
+
+//console.log('hooray');
+
+
 
 function createWindow () {
   // Create the browser window.
@@ -45,6 +68,18 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
+
+
+      //DELETES JSON FILE SO NEXT BOOT DOESN'T HAVE PRESENT. IDEALLY, CHANGE JSON FILE ON CLOSE. THIS WORKS THOUGH.
+      const fs = require('fs');
+
+      fs.unlink('arduino-manager/arduinoSettings.json', (err) => {
+          if (err) throw err;
+          console.log('path/file.txt was deleted');
+      });
+
+
+
     mainWindow = null
   })
 }
